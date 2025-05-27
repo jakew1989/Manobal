8efa49e20684827bca32fe37ac7db2db00f48dbe
jake.wright <jake.wright99@yahoo.com>
2025-05-27 11:14:12 +0100

    moved analytics to main

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,7 @@
 import NavBar from './components/navbar/navbar'
 import { Outlet } from 'react-router-dom'
 import Footer from './components/Footer'
-import { Analytics } from '@vercel/analytics/react';
+
 
 const Layout = () => {
     return (
@@ -9,7 +9,6 @@ const Layout = () => {
             <NavBar/>
             <Outlet/>
             <Footer/>
-            <Analytics />
         </>
     )
 }
